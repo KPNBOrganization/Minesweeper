dffb970b591b042009ed8ec723195f5f50de0227
Unknown <kirill.pilipchuk@gmail.com>
2018-10-06 16:14:32 +0200

    Fixing logical issues

---
+++ src/game.js
@@ -0,0 +1,15 @@
+function Game( ctx )
+{
+
+	// ctx.fillStyle = 'rgb()';
+
+	// Rect draw
+
+	// ctx.fillStyle = '#000000';
+	// ctx.fillRect( 0, 0, 150, 75 );
+
+	// Image draw
+
+	
+
+}
---
+++ src/script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+
+	var canvas = document.getElementById('canvas');
+	var ctx = canvas.getContext('2d');
+
+	var game = new Game( ctx );
+
+};
